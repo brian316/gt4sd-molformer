3f44be3295095b509fe2358aa2fb24efee50fd37
christofid <gchridi@gmail.com>
2023-03-07 13:35:46 +0100

    Fix url

--- install_apex.sh
+++ install_apex.sh
@@ -4,4 +4,4 @@
 pip install torch==1.12.1+cu116 --extra-index-url https://download.pytorch.org/whl/cu116
 
 # install apex
-pip install -v --disable-pip-version-check --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" apex @ git+https://github.com/NVIDIA/apex
+pip install -v --disable-pip-version-check --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" "apex @ git+https://github.com/NVIDIA/apex"
